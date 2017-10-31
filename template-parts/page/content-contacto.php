81ca1063f8c6de70aaafb69ec86166eadc317c22
frkodes <frkalderon@gmail.com>
2017-10-31 15:11:35 -0600

    Contact layout done

---
+++ template-parts/page/content-contacto.php
@@ -0,0 +1,58 @@
+<div class="container-fluid">
+	<div class="row">
+		<div class="main-banner-home contacto">
+			
+			<h1 class="text-center paytone banner-title">
+				ATRACCIONES<br>
+				<span class="roboto">DIVERSIÓN GARANTIZADA</span>
+			</h1>
+
+		</div>
+	</div>
+</div>
+
+
+<div class="maps-container container">
+	<div class="row">
+		
+		<div class="maps-left hidden d-md-block col-md-3">
+			ok
+		</div>
+
+		<div class="maps-right col-xs-12 col-md-9">
+			<div id="map0" class="map d-xs-block">Map - 00</div>
+			<div id="map1" class="map hidden">Map - 01</div>
+			<div id="map2" class="map hidden">Map - 02</div>
+			<div id="map3" class="map hidden">Map - 03</div>
+			<div id="map4" class="map hidden">Map - 04</div>
+			<div id="map5" class="map hidden">Map - 05</div>
+		</div>
+	</div>
+</div>
+
+<div class="container">
+	<div class="row contact-container">
+		<div class="col-xs-12 col-md-6 azul-01 contact-text">
+			<p>En Los Camachos abrimos <strong>TODO el año</strong> de 8 de la mañana a 6 de la tarde.</p>
+			<p>¡Vente con toda la familia a disfrutar del parque acuático!</p> 
+			<p>O llámanos de lunes a viernes de 9 a 2 y de 4 a 6 pm a los teléfonos <strong>01800-080-1982</strong>, <strong>3122-4657</strong> ó <strong>3122-4034</strong>.</p> 
+		</div>
+		<div class="col-xs-12 col-md-6 form">
+			<div class="form">
+				<div class="form-group">
+					<label for="nombre">Nombre</label>
+					<input type="text" name="nombre" class="form-control">
+				</div>
+				<div class="form-group">
+					<label for="correo">Correo</label>
+					<input type="text" name="correo" class="form-control">
+				</div>
+				<div class="form-group">
+					<label for="Mensaje">Escribe aquí tu mensaje</label>
+					<input type="text" name="mensaje" class="form-control">
+				</div>
+				<div class="form-group"><input type="submit" class="btn btn-primary paytone amarillo-00 mayus btn-send" value="Enviar"></div>
+			</div>
+		</div>
+	</div>
+</div>
